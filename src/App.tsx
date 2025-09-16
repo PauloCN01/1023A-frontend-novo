e5ed0e734e5d86f813fe0ace457aabd50f31c8d6
PauloCN01 <paulo.ferreira3@estudante.edu.br>
2025-09-16 11:43:12 -0400

    09-16 formulário

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,40 @@ function App() {
     .then((dados)=>setEstudantes(dados))
   }, [])
   const [estudantes, setEstudantes] = useState<EstudanteType[]>([])
+  const [nome, setNome] = useState("")
+  const [idade, setIdade] = useState(0)
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault()
+    const estudante = {nome, idade}
+    fetch("/api/estudantes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(estudante)
+    })
+    .then((response) => response.json())
+    .then((dados) => {
+      console.log(dados)
+      setEstudantes([...estudantes, dados])
+      setNome("")
+      setIdade(0)
+    })
+  }
+ 
   return (
     <>
+      <h1>Cadastro de Estudantes</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" value={nome}
+         onChange={(e) => setNome(e.target.value)}/>
+
+        <input type="number" placeholder="Idade" value={idade}
+         onChange={(e) => setIdade(Number(e.target.value))}/>
+
+        <input type="submit" value="Cadastrar"/>
+      </form>
       <h1>Lista de Estudantes</h1>
       <div className="container-estudantes">
         {
